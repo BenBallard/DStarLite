1995f41341f0484e55a3592d120b1a351e305c56
Ben Ballard <beanman256@gmail.com>
2011-02-03 23:33:27 -0500

    Added

--- src/simulator.py
+++ src/simulator.py
@@ -11,51 +11,49 @@ import mapper
 import ConfigParser
 import path
 import astar
-import dstar
+#import dstar
 import dstar2
-import Dlite
-import DstarLite
+import dstar3
+#import Dlite
+#import DstarLite
 
 
 '''
 config = ConfigParser.SafeConfigParser()
 config.read("robot.ini")
 '''
-robot = Robot(2,2,3)
+robot = Robot(17,4,3)
 imageMap = ImageReader()
-imageMap.loadFile("/home/ben/TestImage1.bmp")
+imageMap.loadFile("../map.bmp")
 mapper.initalize(imageMap,robot)
 moveGrid = imageMap.convertToGrid().copy()
 
-goal = point(20,20)
+goal = point(3,17)
 
 mapper.printMoveGrid()
 
 print "STARTIN LOOP"
 moveId=0
+Xlength = mapper.grid.shape[0]
+Ylength = mapper.grid.shape[1]
+dstar = dstar3.DStar(Xlength,Ylength,goal)
 
 while (robot.y != goal.y or robot.x != goal.x) :
     moveId = moveId+1
     print moveId
-    
     if path.pathIsBroken(mapper.grid) :
         path.restart()
         print "The path is broken"
-        
       #  dstar2.dstar(mapper, robot, goal, path)
-        Dlite.dstar(robot,goal,path)
+        dstar.dstar(mapper, robot, goal, path)
+      #  Dlite.dstar(robot,goal,path)
       #  DstarLite.dstar(mapper, robot, goal, path)
       #  astar.astar(mapper, robot, goal, path)
-        
-    
     pathNode=path.getNextMove()
     robot.x = pathNode.x
     robot.y = pathNode.y
     mapper.moveGrid[pathNode.x][pathNode.y]="1"
-    
     mapper.printMoveGrid()
-    
     mapper.updateMap(robot)
-    
-
+    raw_input("TEST")
 
